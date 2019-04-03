4a1960e6c49b160dc86250a167bc2af406484cb3
Isaac Davis <isaac.davis@joyent.com>
2019-04-03 22:26:40 +0000

    MANTA-3552 create Triton Prometheus image MANTA-4008 add a core Prometheus service to Manta

---
+++ boot/setup.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright 2019 Joyent, Inc.
+#
+
+#
+# One-time setup of a Triton/Manta prometheus core zone.
+#
+# It is expected that this is run via the standard Triton user-script,
+# i.e. as part of the "mdata:execute" SMF service. That user-script ensures
+# this setup.sh is run once for each (re)provision of the image. However this
+# script should also be idempotent.
+#
+
+export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: '\
+'${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
+
+NODE=/opt/triton/prometheus/build/node/bin/node
+
+ROOT_DIR=/opt/triton/prometheus
+
+#
+# named-related paths. Keep in sync with "bin/prometheus-configure".
+#
+NAMED_DIR=${ROOT_DIR}/named
+NAMED_LOG_DIR=/var/log/named
+
+#
+# CMON key-related paths. Keep in sync with "bin/certgen" and
+# "bin/prometheus-configure".
+#
+CMON_AUTH_DIR=${ROOT_DIR}/keys
+CMON_KEY_FILE=${CMON_AUTH_DIR}/prometheus.key.pem
+CMON_CERT_FILE=${CMON_AUTH_DIR}/prometheus.cert.pem
+
+#
+# Prometheus data that should be persistent across reprovisions is stored on its
+# delegate dataset:
+#
+#   /data/prometheus/
+#       data/    # time-series database
+#
+PERSIST_DIR=/data/prometheus
+DATA_DIR=${PERSIST_DIR}/data
+
+function fatal {
+    printf '%s: ERROR: %s\n' "$(basename ${0})" "${*}" >&2
+    exit 1
+}
+
+#
+# We can't use the sapi config file to determine $FLAVOR yet, because this runs
+# before the SAPI config gets written on zone setup, so we check for the
+# existence of manta_role instead. This is the same method that moray uses for
+# determining $FLAVOR.
+#
+if [[ -n $(mdata-get sdc:tags.manta_role) ]]; then
+    export FLAVOR='manta'
+else
+    export FLAVOR='triton'
+fi
+
+# ---- internal routines
+
+# Mount our delegate dataset at /data.
+function prometheus_setup_delegate_dataset {
+    local mountpoint
+
+    dataset=zones/$(zonename)/data
+    mountpoint=$(zfs get -Ho value mountpoint "${dataset}")
+    if [[ "${mountpoint}" != '/data' ]]; then
+        zfs set mountpoint=/data "${dataset}"
+    fi
+}
+
+function prometheus_setup_env {
+    if [[ ! $(grep prometheus /root/.profile) ]]; then
+        echo '' >>/root/.profile
+        echo "export PATH=${ROOT_DIR}/bin:${ROOT_DIR}/prometheus:\$PATH" \
+            >>/root/.profile
+    fi
+}
+
+function prometheus_setup_named {
+    local localhost_zone
+    local arpa_zone
+
+    svccfg import /opt/local/lib/svc/manifest/bind.xml
+
+    mkdir -p "${NAMED_DIR}"
+    mkdir -p "${NAMED_DIR}/master"
+    mkdir -p "${NAMED_DIR}/slave"
+    mkdir -p "${NAMED_LOG_DIR}"
+
+    read -rd '' localhost_zone <<LOCALHOST_ZONE || true
+\$TTL 3D
+
+\$ORIGIN localhost.
+
+@       1D      IN     SOA     @       root (
+                       0               ; serial
+                       8H              ; refresh
+                       2H              ; retry
+                       4W              ; expiry
+                       1D              ; minimum
+                       )
+
+@       IN      NS      @
+        IN      A       127.0.0.1
+LOCALHOST_ZONE
+
+    read -rd '' arpa_zone <<ARPA_ZONE || true
+\$TTL 3D
+
+@       IN      SOA     localhost. root.localhost. (
+                        0               ; Serial
+                        8H              ; Refresh
+                        2H              ; Retry
+                        4W              ; Expire
+                        1D              ; Minimum TTL
+                        )
+
+       IN      NS      localhost.
+
+1      IN      PTR     localhost.
+ARPA_ZONE
+
+    echo -e "${localhost_zone}" > "${NAMED_DIR}/master/localhost"
+    echo -e "${arpa_zone}" > "${NAMED_DIR}/master/127.in-addr.arpa"
+
+    chown -R named:named \
+        "${NAMED_DIR}" \
+        "${NAMED_LOG_DIR}"
+}
+
+function prometheus_setup_prometheus {
+    mkdir -p "${DATA_DIR}"
+
+    /usr/sbin/svccfg import /opt/triton/prometheus/smf/manifests/prometheus.xml
+
+    #
+    # Set up key and client certificate used to auth with this DC's CMON.
+    #
+    echo 'Generating key and client cert for CMON auth'
+    mkdir -p "${CMON_AUTH_DIR}"
+    "${NODE}" '--abort_on_uncaught_exception' \
+        /opt/triton/prometheus/bin/certgen "${FLAVOR}"
+
+    #
+    # The prometheus SMF service runs as the 'nobody' user, so the files it
+    # accesses must be owned by nobody. Here, we ensure this for the files and
+    # directory that will remain static for the lifetime of the zone.
+    #
+    chown nobody:nobody \
+        "${CMON_KEY_FILE}" \
+        "${CMON_CERT_FILE}" \
+        "${DATA_DIR}" \
+
+    #
+    # prometheus-configure contains the common setup code that must be run here
+    # and also on config-agent updates
+    #
+    TRACE=1 /opt/triton/prometheus/bin/prometheus-configure
+}
+
+# ---- mainline
+
+prometheus_setup_delegate_dataset
+prometheus_setup_env
+
+if [[ "${FLAVOR}" == 'manta' ]]; then
+
+    MANTA_SCRIPTS_DIR=/opt/smartdc/boot/manta-scripts
+    source "${MANTA_SCRIPTS_DIR}/util.sh"
+    source "${MANTA_SCRIPTS_DIR}/services.sh"
+
+    manta_common_presetup
+    manta_add_manifest_dir '/opt/triton/prometheus'
+    manta_common_setup 'prometheus' 0
+
+    prometheus_setup_named
+    prometheus_setup_prometheus
+
+    manta_common_setup_end
+
+else # "$FLAVOR" == "triton"
+
+    CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/triton/prometheus
+    source /opt/smartdc/boot/lib/util.sh
+    sdc_common_setup
+
+    prometheus_setup_named
+    prometheus_setup_prometheus
+
+    # Log rotation.
+    sdc_log_rotation_add config-agent /var/svc/log/*config-agent*.log 1g
+    sdc_log_rotation_add registrar /var/svc/log/*registrar*.log 1g
+    sdc_log_rotation_add prometheus /var/svc/log/*prometheus*.log 1g
+    sdc_log_rotation_setup_end
+
+    sdc_setup_complete
+fi
+
+exit 0
---
+++ tools/obliterate-prometheus-service.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright 2019 Joyent, Inc.
+#
+
+#
+# Obliterate a Triton prometheus service and instances. This is just for
+# development.
+#
+# Usage:
+#       scp tools/obliterate-prometheus-service.sh coal:/var/tmp
+#       ssh coal
+#       /var/tmp/obliterate-prometheus-service.sh
+#
+
+if [[ -n "$TRACE" ]]; then
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+set -o errexit
+set -o pipefail
+
+function fatal() {
+    echo "$0: fatal: \$*" >&2
+    exit 1
+}
+
+function obliterate_prometheus_service {
+    local promSvc
+    local appName=$1
+    local appUuid
+
+    appUuid=$(sdc-sapi /applications?name=$appName | json -H 0.uuid)
+
+    promSvc=$(sdc-sapi "/services?name=prometheus&application_uuid=$appUuid" | \
+        json -H 0.uuid)
+
+    if [[ -z "$promSvc" ]]; then
+        return
+    fi
+
+    sdc-sapi /instances?service_uuid=$promSvc \
+        | json -Ha uuid params.alias \
+        | while read uuid alias; do
+            echo "Delete prometheus instance $uuid ($alias)"
+            sdc-sapi /instances/$uuid -X DELETE
+        done
+
+    echo "Delete prometheus service ($promSvc)"
+    sdc-sapi /services/$promSvc -X DELETE
+}
+
+
+# ---- mainline
+
+# Guard from running this in production. This is the same guard file we use
+# for running many of the Triton test suites.
+if [[ ! -f "/lib/sdc/.sdc-test-no-production-data" ]]; then
+    cat <<EOF
+To run this you must create the following file:
+
+    /lib/sdc/.sdc-test-no-production-data
+
+after ensuring you have no production data in this TritonDC.
+EOF
+    exit 2
+fi
+
+appName=$1
+if [[ $appName != 'sdc' && $appName != 'manta' ]]; then
+    echo "Usage: obliterate-prometheus-service.sh <sdc|manta>"
+    exit 1
+fi
+
+obliterate_prometheus_service $appName
